2b16e4f3ee292ab9d174c5af10eaa1079e904ea5
Derek Hanciles <drhanciles@icloud.com>
2019-01-23 13:40:04 -0700

    jQuery is hooked up and scripts.js file is properly hooked up to the html doc

---
+++ public/js/scripts.js
@@ -0,0 +1,16 @@
+// Functions
+function generateHexValues() {
+  // store all possible hex digits in an array
+  // iterate through array and randomly select digits based on their index position
+  // use math random to generate random hex digits
+  const hexDigits = ['A', 'B', 'C', 'D', 'E', 'F', '0', '1', '2', '3', '4', '5', '6', '8', '9']; 
+  let hevCode = ''
+  for(let i = 0; i < 6; i++) {
+
+  }
+}
+
+// Event Listeners 
+$('.generate-button').on('click', () => {
+  console.log('gerenrate hex values button has been clicked')
+})
