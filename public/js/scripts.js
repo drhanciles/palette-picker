dbb2ef368924ad2cf7d3f5fde74ccb5f026182be
Derek Hanciles <drhanciles@icloud.com>
2019-01-23 15:26:22 -0700

    Implemented function that toggles lock icon on or off and it is working as expected

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,14 +1,22 @@
 // Functions
-getRandomDigits = (num) => (Math.floor(Math.random() * Math.floor(num)))
+getRandomDigits = () => (Math.floor(Math.random() * 16 ))
 
 generateHexValues = () =>  {  
   const hexDigits = ['A', 'B', 'C', 'D', 'E', 'F', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
   let hexValue = ''
   while(hexValue.length < 6 ) {
-    let newValue = getRandomDigits(16)
+    let newValue = getRandomDigits()
     hexValue = hexValue + hexDigits[newValue]
   }
   return hexCode = `#${hexValue}`
+}
+
+toggleLock = () => {
+  if($(event.target).hasClass('fa-lock-open')) {
+    $(event.target).removeClass('fa-lock-open').addClass('fa-lock')
+  } else {
+    $(event.target).removeClass('fa-lock').addClass('fa-lock-open')
+  }
 }
 
 // Event Listeners 
@@ -16,4 +24,9 @@ $('.generate-button').on('click', () => {
   console.log('gerenrate hex values button has been clicked')
   generateHexValues()
   console.log(hexCode)
+})
+
+$('.lock-icon').on('click', () => {
+  console.log('clicked icon')
+  toggleLock()
 })
