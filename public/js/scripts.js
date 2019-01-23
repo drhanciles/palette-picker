cc025f81e1014fecab06d47c50c4fcc3a209268b
Derek Hanciles <drhanciles@icloud.com>
2019-01-23 14:40:57 -0700

    Function that generates hexcodes has been implemented and is working as expected

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,16 +1,19 @@
 // Functions
-function generateHexValues() {
-  // store all possible hex digits in an array
-  // iterate through array and randomly select digits based on their index position
-  // use math random to generate random hex digits
-  const hexDigits = ['A', 'B', 'C', 'D', 'E', 'F', '0', '1', '2', '3', '4', '5', '6', '8', '9']; 
-  let hevCode = ''
-  for(let i = 0; i < 6; i++) {
+getRandomDigits = (num) => (Math.floor(Math.random() * Math.floor(num)))
 
+generateHexValues = () =>  {  
+  const hexDigits = ['A', 'B', 'C', 'D', 'E', 'F', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+  let hexValue = ''
+  while(hexValue.length < 6 ) {
+    let newValue = getRandomDigits(16)
+    hexValue = hexValue + hexDigits[newValue]
   }
+  return hexCode = `#${hexValue}`
 }
 
 // Event Listeners 
 $('.generate-button').on('click', () => {
   console.log('gerenrate hex values button has been clicked')
+  generateHexValues()
+  console.log(hexCode)
 })
