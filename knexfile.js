990701c0d76acfe79579cecbe044b2f3ec52ea39
Derek Hanciles <drhanciles@icloud.com>
2019-01-26 13:48:31 -0700

    Updated development environment in knex.js updated the connection and useNullAsDefault keys

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,9 @@
-// Update with your config settings.
-
 module.exports = {
 
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
+    client: 'pg',
+    connection: 'postgres://localhost/projects', 
+    useNullAsDefault: true
   },
 
   staging: {
