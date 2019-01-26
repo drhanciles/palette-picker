78dbb31b79d9b6dc14d6d87f9eae69ec978a78f1
Derek Hanciles <drhanciles@icloud.com>
2019-01-26 14:35:20 -0700

    Initial migration file has been set up and is working properly

--- db/migrations/20190126135703_initial.js
+++ db/migrations/20190126135703_initial.js
@@ -1,8 +1,31 @@
 
 exports.up = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.createTable('projects', function(table) {
+      table.increments('id').primary(); 
+      table.string('title'); 
+      table.timestamps(true, true); 
+    }), 
+
+    knex.schema.createTable('palettes', function(table) {
+      table.increments('id').primary(); 
+      table.string('title'); 
+      table.string('color_one'); 
+      table.string('color_two'); 
+      table.string('color_three'); 
+      table.string('color_four'); 
+      table.string('color_five'); 
+      table.integer('project_id').unsigned()
+      table.foreign('project_id')
+        .references('projects.id'); 
+      table.timestamps(true, true); 
+    })
+  ])
 };
 
 exports.down = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.dropTable('palettes'), 
+    knex.schema.dropTable('projects')
+  ]); 
 };
